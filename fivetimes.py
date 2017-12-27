aa523f899b06feb154ec233b7182b4b9919f2b27
radavis47 <radavis47@gmail.com>
2017-12-27 16:00:50 -0600

    new files

---
+++ fivetimes.py
@@ -0,0 +1,12 @@
+'''print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+'''
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) +')')
+    i = i + 1
+
+# both sections produce the same code
